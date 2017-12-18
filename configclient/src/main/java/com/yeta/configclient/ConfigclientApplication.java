8c979c8216064c5e997e64d8d53db3036ca1eff1
yeta <429721783@qq.com>
2017-12-18 22:30:03 +0800

    修改

--- configclient/src/main/java/com/yeta/configclient/ConfigclientApplication.java
+++ configclient/src/main/java/com/yeta/configclient/ConfigclientApplication.java
@@ -2,7 +2,11 @@ package com.yeta.configclient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient        //向服务中心注册
+@RefreshScope
 @SpringBootApplication
 public class ConfigclientApplication {
 
--- configserver/src/main/java/com/yeta/configserver/ConfigserverApplication.java
+++ configserver/src/main/java/com/yeta/configserver/ConfigserverApplication.java
@@ -3,8 +3,10 @@ package com.yeta.configserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @EnableConfigServer		//开启配置服务器的功能
+@EnableEurekaClient		//向服务中心注册
 @SpringBootApplication
 public class ConfigserverApplication {
 
